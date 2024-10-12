364079e703e935ce3e80c0627ba19bf41bbcc901
byun0-0 <isly7959@naver.com>
2024-10-12 15:47:28 +0900

    audio 에러 수정

--- src/app/components/QRScan.tsx
+++ src/app/components/QRScan.tsx
@@ -16,14 +16,15 @@ const QRScan: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null); // 비디오 요소를 참조하기 위한 ref
   const [result, setResult] = useState<QRResult | null>(null); // QR 코드 스캔 결과의 타입을 지정
   const [error, setError] = useState<string | null>(null); // 에러 메시지
-  const beepSound = new Audio('/beep.mp3'); // QR 코드 스캔 시 비프음
+  const beepSound = useRef<HTMLAudioElement | null>(null); // 비프음 소리 파일을 위한 ref
   useEffect(() => {
+    beepSound.current = new Audio('/beep.mp3');
     const qrScanner = new QrScanner(videoRef.current!, (scanResult) => {
       try {
         // JSON 파싱
         const parsedResult: QRResult = JSON.parse(scanResult); // 인터페이스 사용
         setResult(parsedResult); // QR 코드 결과를 상태에 저장
-        beepSound.play(); // 비프음 재생
+        beepSound.current?.play(); // 비프음 재생
         setError(null); // 에러 초기화
       } catch (err) {
         console.error('Failed to parse JSON:', err);
