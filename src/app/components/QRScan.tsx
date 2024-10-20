e8d36f8b286ac0f5efcfc50c22ac5e6eec3b4768
byun0-0 <isly7959@naver.com>
2024-10-20 16:01:07 +0900

    비효율적인 반복 제거 함수화, 출력 형식 변경

--- src/app/components/QRScan.tsx
+++ src/app/components/QRScan.tsx
@@ -13,6 +13,7 @@ import Tenwon from '../../../public/image/10won.png';
 interface QRResult {
   price: number;
   product: string | null;
+  hypernym: string | null;
 }
 
 const QRScan: React.FC = () => {
@@ -32,14 +33,12 @@ const QRScan: React.FC = () => {
             priceSound.current.pause();
             priceSound.current.currentTime = 0; // 소리 초기화
           }
-          if (!parsedResult.price) {
 
-          }
-          if (parsedResult.product) {
-          priceSound.current = new Audio(`/sound/${parsedResult.product}.m4a`);
-        } else if (parsedResult.price) {
-          priceSound.current = new Audio(`/sound/s${parsedResult.price}.m4a`);
-        }
+          const audioFile = parsedResult.product
+              ? `/sound/${parsedResult.product}.m4a`
+              : `/sound/s${parsedResult.price}.m4a`;
+
+          priceSound.current = new Audio(audioFile);
           beepSound.current?.play();
           priceSound.current?.play();
           setError(null);
@@ -63,6 +62,40 @@ const QRScan: React.FC = () => {
     }
   }, []);
 
+  const renderPriceImages = (price: number) => {
+    const imageArray = [];
+    const numOfFiveThousand = Math.floor(price / 5000);
+    const numOfThousand = Math.floor(price / 1000);
+    const numOfFiveHundred = Math.floor((price % 1000) / 500);
+    const numOfHundred = Math.floor((price % 500) / 100);
+    const numOfTen = Math.floor((price % 100) / 10);
+
+    for (let i = 0; i < numOfThousand; i++) {
+      imageArray.push(<Image key={`thousand-${i}`} src={Onethousandwon} alt="1000won" width={400}/>);
+    }
+    for (let i = 0; i < numOfFiveHundred; i++) {
+      imageArray.push(<Image key={`fivehundred-${i}`} src={Fivehundredwon} alt="500won" width={200}/>);
+    }
+    for (let i = 0; i < numOfHundred; i++) {
+      imageArray.push(<Image key={`hundred-${i}`} src={Onehundredwon} alt="100won" width={180}/>);
+    }
+    for (let i = 0; i < numOfTen; i++) {
+      imageArray.push(<Image key={`ten-${i}`} src={Tenwon} alt="10won" width={160}/>);
+    }
+    return imageArray;
+  };
+  const renderHypernymCategory = (hypernym: string | null) => {
+    if (!hypernym) return null;
+    if (hypernym.includes('school')) {
+      return '학용품';
+    } else if (hypernym.includes('fruit')) {
+      return '과일';
+    } else if (hypernym.includes('animal')) {
+      return '동물';
+    }
+    return null;
+  };
+
   return (
       <div>
         <div style={{textAlign: 'center', padding: '20px'}}>
@@ -81,155 +114,22 @@ const QRScan: React.FC = () => {
         </div>
 
         {/* QR 코드 결과 출력 및 에러 메시지 */}
-        <div style={{padding: '20px', textAlign: 'center'}}>
-          {error ?
+        <div style={{padding: '40px', textAlign: 'center'}}>
+          {error ? (
               <p style={{color: 'red'}}>{error}</p>
-              : <p>QR 코드 결과: {result?.price}원, {result?.product}{/*{qrjsonResult}*/}</p>}
+          ) : result ? (
+              result.hypernym ? (
+                  <p>{renderHypernymCategory(result.hypernym)} - {result.product}</p>
+              ) : (
+                  <p>{result.price}원, {result.product}</p>
+              )
+          ) : null}
         </div>
 
         {/* 이미지와 결과에 따른 렌더링 */}
         <div style={{textAlign: 'center'}}>
-          {/* 가격에 따라 이미지를 렌더링 */}
-          <hr/>
-          {result && result.product && <Image src={`/image/${result.product}.png`} alt={result.product} width={200}/>}
-          {result && result.price === 500 && <Image src={Fivehundredwon} alt={'500won'} width={200}/>}
-          {result && result.price === 600 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-              </div>
-          )}
-          {result && result.price === 700 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-              </div>
-          )}
-          {result && result.price === 800 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-              </div>
-          )}
-          {result && result.price === 830 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Tenwon} alt={'10won'} width={160}/>
-                <Image src={Tenwon} alt={'10won'} width={160}/>
-                <Image src={Tenwon} alt={'10won'} width={160}/>
-              </div>
-          )}
-          {result && result.price === 900 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-                <Image src={Onehundredwon} alt={'100won'} width={180}/>
-              </div>
-          )}
-          {result && result.price === 1000 && <Image src={Onethousandwon} alt={'1000won'} width={400}/>}
-          {result && result.price === 1500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 2000 && (
-              <div>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 2500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 3000 && (
-              <div>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 4000 && (
-              <div>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 4500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 5000 && <Image src={Fivethousandwon} alt={'5000won'} width={1000}/>}
-          {result && result.price === 5500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 6000 && (
-              <div>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 6500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 7000 && (
-              <div>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 7500 && (
-              <div>
-                <Image src={Fivehundredwon} alt={'500won'} width={200}/>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 8000 && (
-              <div>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
-          {result && result.price === 9000 && (
-              <div>
-                <Image src={Fivethousandwon} alt={'5000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-                <Image src={Onethousandwon} alt={'1000won'} width={400}/>
-              </div>
-          )}
+          {result?.product && <Image src={`/image/${result.product}.png`} alt={result.product} width={400}/>}
+          {result && renderPriceImages(result.price)}
         </div>
       </div>
   );
