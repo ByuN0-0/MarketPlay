d2a67c35fca8df29d13f7ed401e65ee41534f89b
byun0-0 <isly7959@naver.com>
2024-10-20 16:02:25 +0900

    5000원 추가

--- src/app/components/QRScan.tsx
+++ src/app/components/QRScan.tsx
@@ -64,26 +64,40 @@ const QRScan: React.FC = () => {
 
   const renderPriceImages = (price: number) => {
     const imageArray = [];
-    const numOfFiveThousand = Math.floor(price / 5000);
-    const numOfThousand = Math.floor(price / 1000);
-    const numOfFiveHundred = Math.floor((price % 1000) / 500);
-    const numOfHundred = Math.floor((price % 500) / 100);
-    const numOfTen = Math.floor((price % 100) / 10);
+    const numOfFiveThousand = Math.floor(price / 5000);  // 5000원짜리 계산 추가
+    const numOfThousand = Math.floor((price % 5000) / 1000);  // 1000원짜리 계산
+    const numOfFiveHundred = Math.floor((price % 1000) / 500);  // 500원짜리 계산
+    const numOfHundred = Math.floor((price % 500) / 100);  // 100원짜리 계산
+    const numOfTen = Math.floor((price % 100) / 10);  // 10원짜리 계산
 
+    // 5000원짜리 이미지 추가
+    for (let i = 0; i < numOfFiveThousand; i++) {
+      imageArray.push(<Image key={`fivethousand-${i}`} src={Fivethousandwon} alt="5000won" width={1000}/>);
+    }
+
+    // 1000원짜리 이미지 추가
     for (let i = 0; i < numOfThousand; i++) {
       imageArray.push(<Image key={`thousand-${i}`} src={Onethousandwon} alt="1000won" width={400}/>);
     }
+
+    // 500원짜리 이미지 추가
     for (let i = 0; i < numOfFiveHundred; i++) {
       imageArray.push(<Image key={`fivehundred-${i}`} src={Fivehundredwon} alt="500won" width={200}/>);
     }
+
+    // 100원짜리 이미지 추가
     for (let i = 0; i < numOfHundred; i++) {
       imageArray.push(<Image key={`hundred-${i}`} src={Onehundredwon} alt="100won" width={180}/>);
     }
+
+    // 10원짜리 이미지 추가
     for (let i = 0; i < numOfTen; i++) {
       imageArray.push(<Image key={`ten-${i}`} src={Tenwon} alt="10won" width={160}/>);
     }
+
     return imageArray;
   };
+
   const renderHypernymCategory = (hypernym: string | null) => {
     if (!hypernym) return null;
     if (hypernym.includes('school')) {
